3a690108535f22626b6095652f53374c31a661b7
user <sooooooobinn@gmail.com>
2025-12-18 16:53:14 +0900

    feat: Service 작성

--- src/main/java/com/example/gmt_auth/domain/auth/service/AuthService.java
+++ src/main/java/com/example/gmt_auth/domain/auth/service/AuthService.java
@@ -1,4 +1,40 @@
 package com.example.gmt_auth.domain.auth.service;
 
+import com.example.gmt_auth.domain.auth.entity.UserEntity;
+import com.example.gmt_auth.domain.auth.repository.UserRepository;
+import com.example.gmt_auth.global.jwt.JWTUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JWTUtil jwtUtil;
+
+    public void join(String username, String password) {
+        if (userRepository.findByUsername(username).isPresent()) {
+            throw new RuntimeException("이미 존재하는 아이디");
+        }
+
+        UserEntity user = new UserEntity();
+        user.setUsername(username);
+        user.setPassword(passwordEncoder.encode(password));
+
+        userRepository.save(user);
+    }
+
+    public String login(String username, String password) {
+        UserEntity user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("아이디 없음"));
+
+        if (!passwordEncoder.matches(password, user.getPassword())) {
+            throw new RuntimeException("비밀번호 불일치");
+        }
+
+        return jwtUtil.createJwt(username);
+    }
 }
