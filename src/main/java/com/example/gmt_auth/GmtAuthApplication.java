529136b5f32c4b7a5365a5e6e5fb482a0ba6399d
user <sooooooobinn@gmail.com>
2025-12-18 12:18:35 +0900

    init

---
+++ src/main/java/com/example/gmt_auth/GmtAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmt_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmtAuthApplication.class, args);
+    }
+
+}
