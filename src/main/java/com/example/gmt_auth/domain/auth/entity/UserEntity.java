dcea82a2f2b8bf76e7bb8582e8ab1b5c98d1f3c4
user <sooooooobinn@gmail.com>
2025-12-18 13:58:03 +0900

    feat: entity 작성

--- src/main/java/com/example/gmt_auth/domain/auth/entity/UserEntity.java
+++ src/main/java/com/example/gmt_auth/domain/auth/entity/UserEntity.java
@@ -1,4 +1,33 @@
 package com.example.gmt_auth.domain.auth.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
