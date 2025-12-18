2c47a0df0a1058de1f5216f39f4b4c914b1945cc
user <sooooooobinn@gmail.com>
2025-12-18 11:48:02 +0900

    init

---
+++ src/main/java/com/example/gmt_main/GmtMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.gmt_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmtMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmtMainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gmt_main/GmtMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gmt_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmtMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
